0b5da59b94decdd2ce688927bbb8fe20d92fc056
Christoph Hösler <christoph.hoesler@gmail.com>
2011-01-25 16:16:43 +0000

    Failed commit: Default

--- src/main/java/org/asoem/greyfish/utils/AbstractDeepCloneable.java
+++ src/main/java/org/asoem/greyfish/utils/AbstractDeepCloneable.java
@@ -8,37 +8,35 @@ import java.util.HashMap;
 import java.util.Map;
 
 public abstract class AbstractDeepCloneable implements DeepClonable {
-	
-	protected AbstractDeepCloneable() { }
 
-	protected AbstractDeepCloneable(AbstractDeepCloneable srcObj,
-			Map<AbstractDeepCloneable, AbstractDeepCloneable> mapDict) {
-		Preconditions.checkNotNull(srcObj);
-		Preconditions.checkNotNull(mapDict);
-		
-		mapDict.put(srcObj, this);
-	}
+    protected AbstractDeepCloneable() { }
 
-	final public DeepClonable deepClone() {
-		return deepClone(new HashMap<AbstractDeepCloneable, AbstractDeepCloneable>());
-	}
+    final public DeepClonable deepClone() {
+        return deepClone(new HashMap<AbstractDeepCloneable, AbstractDeepCloneable>());
+    }
 
-	protected DeepClonable deepClone(Map<AbstractDeepCloneable, AbstractDeepCloneable> mapDict) {
-		Preconditions.checkNotNull(mapDict);
+    protected DeepClonable deepClone(Map<AbstractDeepCloneable, AbstractDeepCloneable> mapDict) {
+        Preconditions.checkNotNull(mapDict);
 
-		return mapDict.containsKey(this) ? mapDict.get(this) : deepCloneHelper(mapDict);
-	}
+        if (mapDict.containsKey(this))
+            return mapDict.get(this);
+        else {
+            AbstractDeepCloneable ret = deepCloneHelper(mapDict);
+            mapDict.put(this, ret);
+            return ret;
+        }
+    }
 
-	///Classes should override this method
-	///with the following code:
-	/// return new NameOfMyClass(this, mapDict);
-	protected abstract AbstractDeepCloneable deepCloneHelper(Map<AbstractDeepCloneable, AbstractDeepCloneable> mapDict);
-	
-	@SuppressWarnings("unchecked")
-	protected static <T extends DeepClonable> T deepClone(T component, Map<AbstractDeepCloneable, AbstractDeepCloneable> map) {
-		// There must not exist any implementation of DeepClonable which doesn't extend DeepCloneable
-		return (component != null) ? (T) ((AbstractDeepCloneable)component).deepClone(map) : null;
-	}
+    ///Classes should override this method
+    ///with the following code:
+    /// return new NameOfMyClass(this, mapDict);
+    protected abstract AbstractDeepCloneable deepCloneHelper(Map<AbstractDeepCloneable, AbstractDeepCloneable> mapDict);
+
+    @SuppressWarnings("unchecked")
+    protected static <T extends DeepClonable> T deepClone(T component, Map<AbstractDeepCloneable, AbstractDeepCloneable> map) {
+        // There must not exist any implementation of DeepClonable which doesn't extend DeepCloneable
+        return (component != null) ? (T) ((AbstractDeepCloneable)component).deepClone(map) : null;
+    }
 
     protected static <T extends DeepClonable> Iterable<T> deepClone(Iterable<T> components, final Map<AbstractDeepCloneable, AbstractDeepCloneable> map) {
         return Iterables.transform(components, new Function<T, T>() { public T apply(T e) {return deepClone(e, map);}});
