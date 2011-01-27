90f91449c3bce4b11cbd9b33c20a919398857c7c
Christoph Hösler <christoph.hoesler@gmail.com>
2011-01-27 15:30:27 +0000

    Failed commit: Default

--- src/main/java/org/asoem/greyfish/lang/Comparables.java
+++ src/main/java/org/asoem/greyfish/lang/Comparables.java
@@ -7,6 +7,8 @@ import com.google.common.collect.Iterables;
 import java.util.Arrays;
 import java.util.Iterator;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 
 public final class Comparables {
     public static <T extends Comparable<T>> boolean areInOrder(T ... others) {
@@ -32,5 +34,15 @@ public final class Comparables {
         T next = iter.next();
         ImmutableList<T> newTail = ImmutableList.copyOf(iter);
         return head.compareTo(next) <= 0 && areInOrder(next, newTail);
+    }
+
+    /**
+     *
+     * @param a lefthand comparable
+     * @param b righthand comparable
+     * @return a.compareTo(b) if both are not <code>null</code>
+     */
+    public static int compare(Comparable a, Comparable b) {
+        return checkNotNull(a).compareTo(checkNotNull(b));
     }
 }
