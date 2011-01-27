ae2a2e1fcd6832bea31ef0039539dab03ab5ea6e
Christoph Hösler <christoph.hoesler@gmail.com>
2011-01-27 15:30:27 +0000

    Failed commit: Default

--- src/main/java/org/asoem/greyfish/utils/AbstractDeepCloneable.java
+++ src/main/java/org/asoem/greyfish/utils/AbstractDeepCloneable.java
@@ -39,12 +39,12 @@ public abstract class AbstractDeepCloneable<T extends AbstractDeepCloneable<T>> 
     protected abstract T deepCloneHelper(CloneMap map);
 
     @SuppressWarnings("unchecked")
-    protected static <T extends DeepClonable> T deepClone(T component, CloneMap map) {
+    protected static <E extends DeepClonable> E deepClone(E component, CloneMap map) {
         // There must not exist any implementation of DeepClonable which doesn't extend DeepCloneable
-        return (component != null) ? (T) ((AbstractDeepCloneable)component).deepClone(map) : null;
+        return (component != null) ? (E) ((AbstractDeepCloneable)component).deepClone(map) : null;
     }
 
-    protected static <T extends DeepClonable> Iterable<T> deepClone(Iterable<T> components, final CloneMap map) {
-        return Iterables.transform(components, new Function<T, T>() { public T apply(T e) {return deepClone(e, map);}});
+    protected static <E extends DeepClonable> Iterable<E> deepClone(Iterable<E> components, final CloneMap map) {
+        return Iterables.transform(components, new Function<E, E>() { public E apply(E e) {return deepClone(e, map);}});
     }
 }
