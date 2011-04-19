24bc43c441464a3aac1f7560dec4dffa3d5fd2a8
Christoph Hösler <christoph.hoesler@gmail.com>
2011-04-19 13:42:54 +0000

    ADDED: Population Chart IMPLEMENTED: ActionGroup to handle action selections

---
+++ src/main/java/org/asoem/greyfish/utils/Counter.java
@@ -0,0 +1,30 @@
+package org.asoem.greyfish.utils;
+
+/**
+ * User: christoph
+ * Date: 19.04.11
+ * Time: 14:21
+ */
+public class Counter {
+
+    private int n;
+
+    public Counter(int n) {
+        this.n = n;
+    }
+
+    public Counter() {
+    }
+
+    public void increase() {
+        n++;
+    }
+
+    public void decrease() {
+        n--;
+    }
+
+    public int get() {
+        return n;
+    }
+}
