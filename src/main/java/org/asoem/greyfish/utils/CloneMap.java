d8e10cc219f06cbe70507303487f11f3e23b780c
Christoph Hösler <christoph@localhost.(none)>
2011-09-19 22:21:59 +0200

    *** DOES NOT COMPILE ***
    MODIFIED moved all coning responsibility to CloneMap / removed necessity to extend AbstractDeepCloneable

--- src/main/java/org/asoem/greyfish/utils/CloneMap.java
+++ src/main/java/org/asoem/greyfish/utils/CloneMap.java
@@ -5,22 +5,36 @@ import com.google.common.collect.ForwardingMap;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Maps;
 
+import javax.annotation.Nullable;
 import java.util.Map;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
 
 public class CloneMap extends ForwardingMap<DeepCloneable, DeepCloneable> {
 
     private final Map<DeepCloneable, DeepCloneable> map = Maps.newHashMap();
+    private DeepCloneable keyForExpectedPut = null;
 
-    public <T extends DeepCloneable, E> E clone(T clonable, Class<E> clazz) {
-        if (clonable == null)
+    public <T extends DeepCloneable, E> E clone(@Nullable T cloneable, Class<E> clazz) {
+        checkNotNull(clazz);
+        if (putExpected())
+            throw new IllegalStateException("A clone should add an entry for itself first with map.insert(clone, this)");
+
+        if (cloneable == null)
             return null;
-        else if (map.containsKey(clonable))
-            return clazz.cast(map.get(clonable));
-        else
-            return clazz.cast(clonable.deepCloneHelper(this));
+        else if (map.containsKey(cloneable)) {
+            return clazz.cast(map.get(cloneable));
+        }
+        else {
+            keyForExpectedPut = cloneable;
+            return clazz.cast(cloneable.deepCloneHelper(this));
+        }
     }
 
-    public static CloneMap newInstance() { return new CloneMap(); }
+    private boolean putExpected() {
+        return keyForExpectedPut != null;
+    }
 
     @Override
     protected Map<DeepCloneable, DeepCloneable> delegate() {
@@ -34,20 +48,35 @@ public class CloneMap extends ForwardingMap<DeepCloneable, DeepCloneable> {
 
     @Override
     public DeepCloneable put(DeepCloneable key, DeepCloneable value) {
-        return super.put(key, value);
+        throw new UnsupportedOperationException();
+    }
+
+    public <E extends DeepCloneable> void insert(E key, E value) {
+        checkNotNull(key);
+        checkNotNull(value);
+        checkArgument(key == keyForExpectedPut, "Key must be the cloneable of your previous clone(cloneable, clone)");
+        checkArgument(key.getClass().equals(value.getClass()), "Class of ");
+        keyForExpectedPut = null;
+        super.put(key, value);
     }
 
     @Override
     public void putAll(Map<? extends DeepCloneable, ? extends DeepCloneable> map) {
-        super.putAll(map);
+        throw new UnsupportedOperationException();
     }
 
     public <T extends DeepCloneable> Iterable<T> cloneAll(Iterable<? extends T> iterable, final Class<T> clazz) {
+        checkNotNull(clazz);
         return Iterables.transform(iterable, new Function<T, T>() {
             @Override
-            public T apply(T clonable) {
-                return CloneMap.this.clone(clonable, clazz);
+            public T apply(T cloneable) {
+                return CloneMap.this.clone(cloneable, clazz);
             }
         });
+    }
+
+    public static <E extends DeepCloneable> E deepClone(@Nullable DeepCloneable cloneable, Class<E> clazz) {
+        checkNotNull(clazz);
+        return new CloneMap().clone(cloneable, clazz);
     }
 }
--- src/main/java/org/asoem/greyfish/utils/DeepCloneable.java
+++ src/main/java/org/asoem/greyfish/utils/DeepCloneable.java
@@ -1,6 +1,5 @@
 package org.asoem.greyfish.utils;
 
 public interface DeepCloneable {
-	public <T extends DeepCloneable> T deepClone(Class<T> clazz);
     public DeepCloneable deepCloneHelper(CloneMap map);
 }
