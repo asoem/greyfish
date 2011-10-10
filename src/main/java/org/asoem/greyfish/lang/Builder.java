6c14f9700fad6b25ad594937dd5c107ea5cc6fdb
Christoph Hösler <christoph@localhost.(none)>
2011-10-10 19:10:40 +0200

    MODIFIED: Transformed MessageTemplate from class to interface. Predefined templates have static factory methods in MessageTemplates ADDED: Mating interaction test succeeds REMOVED: AgentLogger as a member of Agents. Logging needs redesign anyway. MODIFIED: AbstractBuilder implements Builder and has its own build() function

---
+++ src/main/java/org/asoem/greyfish/lang/Builder.java
@@ -0,0 +1,15 @@
+package org.asoem.greyfish.lang;
+
+/**
+ * User: christoph
+ * Date: 20.01.11
+ * Time: 16:12
+ */
+public interface Builder<T> {
+    /**
+     * Create a new instance of {@code E} based on this  builder of type {@code T}
+     * @return a new instance of {@code E}
+     * @throws IllegalStateException if this Builder is not in a state to produce a valid instance of {@code T}
+     */
+    T build() throws IllegalStateException;
+}
