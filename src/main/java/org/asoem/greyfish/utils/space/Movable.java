e154be6ccbcdd860ff9611b061128076fac1cc53
Christoph Hösler <christoph.hoesler@gmail.com>
2011-11-07 16:26:35 +0100

    IN_PROGRESS: Testing the usage of a Monitor in Interactive Simulations.

--- src/main/java/org/asoem/greyfish/utils/space/Movable.java
+++ src/main/java/org/asoem/greyfish/utils/space/Movable.java
@@ -2,24 +2,5 @@ package org.asoem.greyfish.utils.space;
 
 
 public interface Movable {
-
-    public MotionVector2D getMotionVector();
-
-    /**
-     * Change this object's motion vector by rotating it {@code angle}*PI degrees
-     * and adding {@code velocity} to it's length
-     * @param angle The value to sum to the angle of this object's motion vector
-     * @param velocity The value to sum to the length of this object's motion vector
-     */
-    public void changeMotion(double angle, double velocity);
-
-    /**
-     * Change this object's motion vector by rotating it's orientation to {@code angle}*PI degrees
-     * and it's length to {@code velocity}
-     * @param angle The new value of the angle of this object's motion vector
-     * @param velocity The new value of the length of this object's motion vector
-     */
-    public void setMotion(double angle, double velocity);
-
-    public void setOrientation(double alpha);
+    MotionVector2D getMotionVector();
 }
--- src/main/java/org/asoem/greyfish/utils/space/MutableMovable.java
+++ src/main/java/org/asoem/greyfish/utils/space/MutableMovable.java
@@ -10,9 +10,12 @@ public class MutableMovable implements Movable {
 
     public MutableMovable() {}
 
-    @Override
     public void setOrientation(double alpha) {
         orientationAngle = (orientationAngle + alpha) % MathLib.PI;
+    }
+
+    public void setSpeed(double speed) {
+        setMotion(orientationAngle, speed);
     }
 
     @Override
@@ -20,12 +23,22 @@ public class MutableMovable implements Movable {
         return motionVector;
     }
 
-    @Override
+    /**
+     * Change this object's motion vector by rotating it {@code angle}*PI degrees
+     * and adding {@code velocity} to it's length
+     * @param angle The value to sum to the angle of this object's motion vector
+     * @param velocity The value to sum to the length of this object's motion vector
+     */
     public void changeMotion(double angle, double velocity) {
         motionVector = motionVector.moved(angle, velocity);
     }
 
-    @Override
+    /**
+     * Change this object's motion vector by rotating it's orientation to {@code angle}*PI degrees
+     * and it's length to {@code velocity}
+     * @param angle The new value of the angle of this object's motion vector
+     * @param velocity The new value of the length of this object's motion vector
+     */
     public void setMotion(double angle, double velocity) {
         motionVector = ImmutableMotionVector.of(angle, velocity);
     }
