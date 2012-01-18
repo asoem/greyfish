e77b8ef6ac2072e45245af5b68f074a219375d2e
Christoph Hösler <christoph.hoesler@gmail.com>
2012-01-18 17:08:35 +0100

    IN_PROGRESS: On the way to a ConditionTreeModel and a ConditionTreePresentationModel. Changing the view on our way as well.

--- src/main/java/org/asoem/greyfish/utils/base/DeepCloner.java
+++ src/main/java/org/asoem/greyfish/utils/base/DeepCloner.java
@@ -8,6 +8,13 @@ import java.util.Map;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
+/**
+ * A {@code DeepCloner} is used to make deep copies of {@link DeepCloneable} objects.
+ *
+ * The usual way is to use the static {@link #clone(DeepCloneable, Class)} method to clone a certain object.
+ * This method calls the {@link DeepCloneable#deepClone(DeepCloner)} method of the object whose implementation should do nothing but call the "Cloner"-Constructor.
+ * Here, the object should pass all fields to the {@link #cloneField(DeepCloneable, Class)} method, which are required for a deep copy.
+ */
 public class DeepCloner {
 
     private final Map<DeepCloneable, DeepCloneable> map = Maps.newHashMap();
