3dce194d1fec8b5458143b38b0306112a611454d
Christoph Hösler <christoph.hoesler@gmail.com>
2012-04-13 09:13:33 +0200

    CHANGED: Agent event information is logged directly through the SimulationLogger without an intermediate AgentEvent  IN_PROGRESS: Trying out different SimulationLogger implementations

---
+++ src/main/java/org/asoem/greyfish/utils/base/Exceptions.java
@@ -0,0 +1,20 @@
+package org.asoem.greyfish.utils.base;
+
+/**
+ * User: christoph
+ * Date: 12.04.12
+ * Time: 14:44
+ *
+ * Taken from http://www.eishay.com/2011/11/throw-undeclared-checked-exception-in.html
+ */
+public class Exceptions {
+    public static RuntimeException asRuntimeException(Exception e) {
+        Exceptions.<RuntimeException>throwAs(e);
+        return null;
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <E extends Throwable> void throwAs(Throwable e) throws E {
+        throw (E)e;
+    }
+}
