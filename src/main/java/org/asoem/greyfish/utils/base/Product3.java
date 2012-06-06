c7549b77865c586328a3ea83f6fbe93b0478f992
Christoph Hösler <christoph.hoesler@gmail.com>
2012-06-06 18:01:53 +0200

    CHANGED: Mereged Zipped classes into the Tuple classes

---
+++ src/main/java/org/asoem/greyfish/utils/base/Product3.java
@@ -0,0 +1,14 @@
+package org.asoem.greyfish.utils.base;
+
+/**
+ * User: christoph
+ * Date: 05.06.12
+ * Time: 12:43
+ */
+public interface Product3<E1, E2, E3> {
+    E1 _1();
+
+    E2 _2();
+
+    E3 _3();
+}
---
+++ src/test/java/org/asoem/greyfish/utils/base/Tuple3Test.java
@@ -0,0 +1,40 @@
+package org.asoem.greyfish.utils.base;
+
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.Set;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+/**
+ * User: christoph
+ * Date: 06.06.12
+ * Time: 11:54
+ */
+public class Tuple3Test {
+    @Test
+    public void testZipped() throws Exception {
+        // given
+        final Tuple3<Set<String>, Set<Double>, Set<Date>> tuple3
+                = Tuple3.of(Collections.singleton("a"), Collections.singleton(1.0), Collections.singleton(new Date()));
+
+        // when
+        final Tuple3.Zipped<String, Double, Date> zipped = tuple3.zipped();
+
+
+        // then
+        assertThat(zipped).hasSize(1);
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void testZippedIllegal() throws Exception {
+        // given
+        final Tuple3<String, Double, Date> tuple3
+                = Tuple3.of("a", 1.0, new Date());
+
+        // when
+        final Tuple3.Zipped<String, Double, Date> zipped = tuple3.zipped();
+    }
+}
