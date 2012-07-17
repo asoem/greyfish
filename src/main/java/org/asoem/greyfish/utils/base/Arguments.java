9ee28c51a8765746cdb858ad0b85f72ebd25165c
Christoph Hösler <christoph.hoesler@gmail.com>
2012-07-17 16:42:16 +0200

    CHANGED: Introduced an Arguments class for Callback arguments CHANGED: Renamed Some Action classes ADDED: Mating assortment to SimpleBranchingModel

---
+++ src/main/java/org/asoem/greyfish/utils/base/Arguments.java
@@ -0,0 +1,11 @@
+package org.asoem.greyfish.utils.base;
+
+import java.util.Map;
+
+/**
+ * User: christoph
+ * Date: 17.07.12
+ * Time: 11:40
+ */
+public interface Arguments extends Map<String, Object> {
+}
---
+++ src/main/java/org/asoem/greyfish/utils/base/Callbacks.java
@@ -0,0 +1,78 @@
+package org.asoem.greyfish.utils.base;
+
+import com.google.common.collect.ImmutableMap;
+import org.simpleframework.xml.Element;
+
+import javax.annotation.Nullable;
+
+/**
+ * User: christoph
+ * Date: 15.05.12
+ * Time: 16:13
+ */
+public class Callbacks {
+
+    private static final ArgumentMap ZERO_ARGUMENTS = new ArgumentMap(ImmutableMap.<String, Object>of());
+
+    public static <T> Callback<Object, T> constant(final T returnValue) {
+        return new ConstantCallback<T>(returnValue);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> Callback<T, Void> emptyCallback() {
+        return (Callback<T, Void>) EmptyCallback.SINGLE_INSTANCE;
+    }
+
+    public static <C, T> T call(Callback<C, T> callback, C caller) {
+        return callback.apply(caller, ZERO_ARGUMENTS);
+    }
+
+    private static enum EmptyCallback implements Callback<Object, Void> {
+        SINGLE_INSTANCE;
+
+        @Override
+        public Void apply(Object caller, Arguments arguments) {
+            return null;
+        }
+    }
+
+    private static class ConstantCallback<T> implements Callback<Object, T> {
+
+        @Element(name = "value", required = false)
+        @Nullable
+        private final T value;
+
+        public ConstantCallback(@Element(name = "value", required = false) T returnValue) {
+            this.value = returnValue;
+        }
+
+        @Override
+        public T apply(Object caller, Arguments arguments) {
+            return value;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof ConstantCallback)) return false;
+
+            ConstantCallback that = (ConstantCallback) o;
+
+            if (value != null ? !value.equals(that.value) : that.value != null) return false;
+
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            return value != null ? value.hashCode() : 0;
+        }
+
+        @Override
+        public String toString() {
+            return "ConstantCallback{" +
+                    "value=" + value +
+                    '}';
+        }
+    }
+}
