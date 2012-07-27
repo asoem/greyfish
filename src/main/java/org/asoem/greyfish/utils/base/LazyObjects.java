6cd104b3754f32f6cd93032fb9b3f0c88bd942bc
Christoph Hösler <christoph.hoesler@gmail.com>
2012-07-27 18:08:10 +0200

    CHANGED: Using synchronized rather that reentrant lock for LazyObjects as caliper benchmark reports better performance

--- src/main/java/org/asoem/greyfish/utils/base/LazyObjects.java
+++ src/main/java/org/asoem/greyfish/utils/base/LazyObjects.java
@@ -2,6 +2,7 @@ package org.asoem.greyfish.utils.base;
 
 import com.google.common.base.Supplier;
 
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
@@ -11,39 +12,27 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
  * Time: 10:56
  */
 public class LazyObjects {
-    public static <T> LazyObject<T> threadSave(final LazyObject<T> delegate) {
+
+    public static <T> LazyObject<T> synchronizedLazyObject(final LazyObject<T> delegate) {
         return new LazyObject<T>() {
-
-            private final ReadWriteLock lock = new ReentrantReadWriteLock();
-
             @Override
             public void updateValue() {
-                if (isOutdated()) {
-                    lock.readLock().unlock();
-                    lock.writeLock().lock();
-                    try {
-                        delegate.updateValue();
-                    } finally {
-                        lock.writeLock().unlock();
-                        lock.readLock().lock();
-                    }
+                synchronized (delegate) {
+                    delegate.updateValue();
                 }
             }
 
             @Override
             public boolean isOutdated() {
-                return delegate.isOutdated();
+                synchronized (delegate) {
+                    return delegate.isOutdated();
+                }
             }
 
             @Override
             public T get() {
-                lock.readLock().lock();
-                try {
-                    updateValue();
+                synchronized (delegate) {
                     return delegate.get();
-                }
-                finally {
-                    lock.readLock().unlock();
                 }
             }
         };
@@ -51,5 +40,9 @@ public class LazyObjects {
 
     public static <T> LazyObject<T> computeOnce(Supplier<T> supplier) {
         return new LazyObjectImpl<T>(supplier, UpdateRequests.updateOnce());
+    }
+
+    public static void main(String[] args) {
+
     }
 }
