c59f3e5f7ca9ee302044e9a010d52c4529dfc27d
Christoph Hösler <christoph.hoesler@gmail.com>
2012-08-24 19:33:41 +0200

    REMOVED: close() method from SimulationLogger CHANGED: Switched to JOptSimple from commons.cli for command line option parsing

--- src/main/java/org/asoem/greyfish/cli/GreyfishCLIApplication.java
+++ src/main/java/org/asoem/greyfish/cli/GreyfishCLIApplication.java
@@ -8,26 +8,29 @@ import com.google.common.io.Closeables;
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Inject;
+import com.google.inject.Module;
 import com.google.inject.name.Named;
 import com.google.inject.name.Names;
-import org.apache.commons.cli.*;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
 import org.asoem.greyfish.core.inject.CoreModule;
 import org.asoem.greyfish.core.simulation.ParallelizedSimulation;
 import org.asoem.greyfish.core.simulation.ParallelizedSimulationFactory;
 import org.asoem.greyfish.core.simulation.SimulationTemplate;
 import org.asoem.greyfish.core.simulation.Simulations;
-import org.asoem.greyfish.models.SimulationTemplateFactory;
+import org.asoem.greyfish.models.Model;
 import org.asoem.greyfish.utils.logging.SLF4JLogger;
 import org.asoem.greyfish.utils.logging.SLF4JLoggerFactory;
 
 import javax.annotation.Nullable;
 import java.io.*;
 import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-import static org.asoem.greyfish.cli.GreyfishCLIApplication.State.*;
+import static java.util.Arrays.asList;
 
 /**
  * User: christoph
@@ -38,10 +41,16 @@ public class GreyfishCLIApplication {
 
     private static final SLF4JLogger LOGGER = SLF4JLoggerFactory.getLogger(GreyfishCLIApplication.class);
 
-    private GreyfishCLIApplication.State state = STARTUP;
+    private static enum State {
+        STARTUP,
+        RUNNING,
+        SHUTDOWN
+    }
+
+    private State state = State.STARTUP;
 
     @Inject
-    private GreyfishCLIApplication(SimulationTemplateFactory simulationTemplateFactory,
+    private GreyfishCLIApplication(Model model,
                                    @Nullable @Named("steps") final Integer steps,
                                    @Named("verbose") final boolean verbose,
                                    @Named("parallelizationThreshold") int parallelizationThreshold) {
@@ -56,55 +65,25 @@ public class GreyfishCLIApplication {
             });
         }
 
-        LOGGER.info("Creating simulation for model {}", simulationTemplateFactory);
+        LOGGER.info("Creating simulation for model {}", model);
         final ParallelizedSimulationFactory simulationFactory = new ParallelizedSimulationFactory(parallelizationThreshold);
-        final SimulationTemplate simulationTemplate = simulationTemplateFactory.get();
+        final SimulationTemplate simulationTemplate = model.createTemplate();
         assert simulationTemplate != null;
         final ParallelizedSimulation simulation = simulationTemplate.createSimulation(simulationFactory);
 
         if (verbose) {
-            final Runnable simulationMonitorTask = new Runnable() {
-                @Override
-                public void run() {
-                    File verboseFile = null;
-                    PrintWriter writer = null;
-                    try {
-                        verboseFile = File.createTempFile("greyfish_verbose_", ".txt");
-                        writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new FileOutputStream(verboseFile), "UTF-8")), true);
-                        LOGGER.info("Writing verbose output to file {}", verboseFile.getAbsolutePath());
-                        while (state == STARTUP)
-                            Thread.sleep(10);
-
-                        while (state == RUNNING) {
-                            writer.println(simulation.getStep() + " - " + simulation.countAgents());
-                            Thread.sleep(1000);
-                        }
-                    } catch (InterruptedException e) {
-                        LOGGER.error("Simulation polling thread got interrupted");
-                    } catch (IOException e) {
-                        LOGGER.error("Could not write verbose output to file {}", verboseFile, e);
-                    } finally {
-                        Closeables.closeQuietly(writer);
-                    }
-                }
-            };
-            Executors.newSingleThreadExecutor().execute(simulationMonitorTask);
+            startSimulationMonitor(simulation);
         }
 
-        LOGGER.info("Model properties:\n\t{}", Joiner.on("\n\t").withKeyValueSeparator("=").join(simulationTemplateFactory.getModelProperties()));
         LOGGER.info("Starting {}", simulation);
-
         final Runnable simulationTask = new Runnable() {
             @Override
             public void run() {
+                state = State.RUNNING;
                 Simulations.runWhile(simulation, Predicates.and(predicateList));
             }
         };
-
         final Future<?> future = Executors.newSingleThreadExecutor().submit(simulationTask);
-
-        state = RUNNING;
-
         try {
             future.get();
         } catch (InterruptedException e) {
@@ -113,126 +92,137 @@ public class GreyfishCLIApplication {
             LOGGER.error("Exception occurred while executing simulation", e);
         }
         finally {
-            if (LOGGER.isInfoEnabled())
-                LOGGER.info("Shutting down simulation {}", simulation);
+            LOGGER.info("Shutting down simulation {}", simulation);
             simulation.shutdown();
-            state = SHUTDOWN;
+            state = State.SHUTDOWN;
         }
+    }
+
+    private void startSimulationMonitor(final ParallelizedSimulation simulation) {
+        final Runnable simulationMonitorTask = new Runnable() {
+            @Override
+            public void run() {
+                File verboseFile = null;
+                PrintWriter writer = null;
+                try {
+                    verboseFile = File.createTempFile("greyfish_verbose_", ".txt");
+                    writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new FileOutputStream(verboseFile), "UTF-8")), true);
+                    LOGGER.info("Writing verbose output to file {}", verboseFile.getAbsolutePath());
+                    while (state == State.STARTUP)
+                        Thread.sleep(10);
+
+                    while (state == State.RUNNING) {
+                        writer.println(simulation.getStep() + " - " + simulation.countAgents());
+                        Thread.sleep(1000);
+                    }
+                } catch (InterruptedException e) {
+                    LOGGER.error("Simulation polling thread got interrupted");
+                } catch (IOException e) {
+                    LOGGER.error("Could not write verbose output to file {}", verboseFile, e);
+                } finally {
+                    Closeables.closeQuietly(writer);
+                }
+            }
+        };
+        Executors.newSingleThreadExecutor().execute(simulationMonitorTask);
     }
 
     public static void main(final String[] args) {
-        try {
-            final CommandLineParser parser = new PosixParser();
-            final Options options = createOptions();
-            final CommandLine line = parser.parse(options, args);
-            processCommandLine(line, options);
-        } catch (ParseException e) {
-            System.out.println("Unexpected exception:" + e.getMessage());
-            System.exit(1);
-        }
-    }
+        // --steps 0 -pt 500 -v -DcompetitionKernelSigma=0.5 org.asoem.greyfish.models.EvolutionOfAssortativeMating
 
-    private static void processCommandLine(final CommandLine line, final Options options) {
-        if (line.hasOption("help")) {
-            usage("", options);
+        final OptionParser optionParser = createOptionParser();
+        final OptionSet optionSet = optionParser.parse(args);
+
+        if (optionSet.has("h") || optionSet.has("?")) {
+            printHelp(optionParser);
             System.exit(0);
         }
 
-        final AbstractModule commandLineModule = new AbstractModule() {
-            @SuppressWarnings("unchecked")
+        final Module commandLineModule = createCommandLineModule(optionSet, new OptionExceptionHandler() {
+
             @Override
-            protected void configure() {
-                if (line.hasOption("scenario")) {
-                    try {
-                        final Class<? extends SimulationTemplateFactory> modelFactoryClass =
-                                (Class<? extends SimulationTemplateFactory>) Class.forName(line.getOptionValue("scenario"));
-                        bind(SimulationTemplateFactory.class).to(modelFactoryClass);
-                    } catch (ClassNotFoundException e) {
-                        System.err.println("Could not find class " + line.getOptionValue("scenario"));
-                        System.exit(1);
-                    }
-                }
-                else {
-                    usage("Option --scenario is mandatory", options);
-                    System.exit(1);
-                }
-
-                ModelParameters.bindProperties(binder(), line.getOptionProperties("D"));
-
-                bind(Integer.class).annotatedWith(Names.named("steps")).toInstance(
-                        line.hasOption("steps") ? Integer.valueOf(line.getOptionValue("steps")) : null);
-
-                bind(Boolean.class).annotatedWith(Names.named("verbose")).toInstance(
-                        line.hasOption("verbose"));
-
-                bind(Integer.class).annotatedWith(Names.named("parallelizationThreshold")).toInstance(
-                        line.hasOption("parallelizationThreshold") ? Integer.valueOf(line.getOptionValue("parallelizationThreshold")) : 1000);
+            public void handle(@Nullable String message) {
+                if (message != null)
+                    System.out.println(message);
+                printHelp(optionParser);
+                System.exit(1);
             }
-        };
+        });
 
         Guice.createInjector(
                 new CoreModule(),
                 commandLineModule)
                 .getInstance(GreyfishCLIApplication.class);
+
+        System.exit(0);
     }
 
-    private static void usage(String s, Options options) {
-        final HelpFormatter formatter = new HelpFormatter();
-        System.out.println(s);
-        formatter.printHelp( GreyfishCLIApplication.class.getSimpleName(), options );
+    private static void printHelp(OptionParser optionParser) {
+        System.out.println("Usage: greyfish [Options] <ModelClass>");
+        try {
+            optionParser.printHelpOn(System.out);
+        } catch (IOException e) {
+            // NOP
+        }
     }
 
-    @SuppressWarnings("AccessStaticViaInstance")
-    private static Options createOptions() {
-        final Option scenarioClass = OptionBuilder
-                .withLongOpt("scenario")
-                .hasArg()
-                .withArgName("CLASS")
-                .withDescription("run given scenario CLASS")
-                .create("c");
+    private static Module createCommandLineModule(final OptionSet optionSet, final OptionExceptionHandler optionExceptionHandler) {
+        return new AbstractModule() {
+            @SuppressWarnings("unchecked")
+            @Override
+            protected void configure() {
 
-        final Option steps = OptionBuilder
-                .withLongOpt("steps")
-                .hasArg()
-                .withArgName("MAX")
-                .withDescription("stop simulation after MAX steps")
-                .create("s");
+                if (optionSet.nonOptionArguments().size() != 1) {
+                    optionExceptionHandler.handle("A single Model CLASS is required");
+                }
 
-        final Option verbose = OptionBuilder
-                .withLongOpt("verbose")
-                .withDescription("Enable verbose mode")
-                .create("v");
+                final String modelClassName = optionSet.nonOptionArguments().get(0);
 
-        final Option property  = OptionBuilder
-                .withArgName("property=value")
-                .hasArgs(2)
-                .withValueSeparator()
-                .withDescription( "use value for given scenario-property" )
-                .create( "D" );
+                try {
+                    final Class<? extends Model> modelFactoryClass =
+                            (Class<? extends Model>) Class.forName(modelClassName);
+                    bind(Model.class).to(modelFactoryClass);
+                } catch (ClassNotFoundException e) {
+                    optionExceptionHandler.handle("Could not find class " + modelClassName);
+                }
 
-        final Option parallelizationThreshold = OptionBuilder
-                .withLongOpt("parallelizationThreshold")
-                .withArgName( "THRESHOLD" )
-                .hasArg()
-                .withDescription("")
-                .withType(Number.class)
-                .create("pt");
+                if (optionSet.has("D")) {
+                    final Properties properties = new Properties();
+                    try {
+                        properties.load(new StringReader(Joiner.on("\n").join(optionSet.valuesOf("D"))));
+                    } catch (IOException e) {
+                        throw new IOError(e);
+                    }
+                    ModelParameters.bindProperties(binder(), properties);
+                }
 
-        final Option help = new Option("h", "help", false, "Print help");
-
-        final Options options = new Options();
-        options.addOption(scenarioClass);
-        options.addOption(steps);
-        options.addOption(help);
-        options.addOption(property);
-        options.addOption(verbose);
-        options.addOption(parallelizationThreshold);
-        return options;
+                bind(Integer.class).annotatedWith(Names.named("steps"))
+                        .toInstance((Integer) optionSet.valueOf("steps"));
+                bind(Boolean.class).annotatedWith(Names.named("verbose"))
+                        .toInstance(optionSet.has("verbose"));
+                bind(Integer.class).annotatedWith(Names.named("parallelizationThreshold"))
+                        .toInstance((Integer) optionSet.valueOf("pt"));
+                bind(File.class).annotatedWith(Names.named("database_dir"))
+                        .toInstance((File) optionSet.valueOf("db"));
+            }
+        };
     }
 
-    public enum State {
-        STARTUP,
-        RUNNING,
-        SHUTDOWN
+    @SuppressWarnings("unchecked")
+    private static OptionParser createOptionParser() {
+        final OptionParser parser = new OptionParser();
+
+        parser.accepts("steps", "stop simulation after MAX steps").withRequiredArg().ofType(Integer.class).defaultsTo(-1);
+        parser.accepts("verbose", "Enable verbose mode").withOptionalArg().ofType(File.class);
+        parser.accepts("D", "set model parameter for given model class").withRequiredArg().describedAs("key=value");
+        parser.accepts("pt", "Set parallelization threshold").withRequiredArg().ofType(Integer.class).defaultsTo(1000);
+        parser.accepts("db", "Set directory where database is written to").withRequiredArg().defaultsTo("./").ofType(File.class);
+        parser.acceptsAll(asList("h", "?"), "Print this help");
+
+        return parser;
+    }
+
+    private static interface OptionExceptionHandler {
+        void handle(String message);
     }
 }
