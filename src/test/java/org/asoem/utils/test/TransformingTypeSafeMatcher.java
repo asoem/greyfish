9b600192a59ab465a0254370ea762114dd33b2d6
Christoph Hösler <christoph.hoesler@gmail.com>
2012-11-02 15:03:52 +0100

    ADDED: Added serialization support to all branch conditions

---
+++ src/test/java/org/asoem/utils/test/TransformingTypeSafeMatcher.java
@@ -0,0 +1,38 @@
+package org.asoem.utils.test;
+
+import com.google.common.base.Function;
+import org.hamcrest.CustomTypeSafeMatcher;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+
+/**
+* User: christoph
+* Date: 02.11.12
+* Time: 14:56
+*/
+public class TransformingTypeSafeMatcher<I, O> extends CustomTypeSafeMatcher<I> {
+
+    private final Function<I, O> transformation;
+    private final Matcher<O> transformMatcher;
+
+    public TransformingTypeSafeMatcher(String description, Function<I, O> transformation, Matcher<O> transformMatcher) {
+        super(description);
+        this.transformation = transformation;
+        this.transformMatcher = transformMatcher;
+    }
+
+    @Override
+    protected boolean matchesSafely(I item) {
+        return transformMatcher.matches(transformation.apply(item));
+    }
+
+    @Override
+    protected void describeMismatchSafely(I item, Description mismatchDescription) {
+        mismatchDescription.appendText("requires that ");
+        transformMatcher.describeMismatch(transformation.apply(item), mismatchDescription);
+    }
+
+    public static <I,O> TransformingTypeSafeMatcher<I, O> has(String description, Function<I, O> transformation, Matcher<O> transformMatcher) {
+        return new TransformingTypeSafeMatcher<I, O>(description, transformation, transformMatcher);
+    }
+}
