0c355a754671aff06a97f49fb0a85da815de77fc
Christoph Hösler <christoph.hoesler@gmail.com>
2012-11-07 08:41:34 +0100

    ADDED: Serialization Support for Agents REFACTORED: Agent construction REFACTORED: Removed ComponentLists in favour of generic SearchableLists

--- src/main/java/org/asoem/greyfish/utils/collect/AugmentedList.java
+++ src/main/java/org/asoem/greyfish/utils/collect/AugmentedList.java
@@ -1,11 +1,9 @@
 package org.asoem.greyfish.utils.collect;
 
-import java.util.List;
-
 /**
  * User: christoph
  * Date: 23.09.12
  * Time: 12:07
  */
-public interface AugmentedList<E> extends List<E>, Searchable<E> {
+public interface AugmentedList<E> extends SearchableList<E> {
 }
---
+++ src/main/java/org/asoem/greyfish/utils/collect/SearchableList.java
@@ -0,0 +1,11 @@
+package org.asoem.greyfish.utils.collect;
+
+import java.util.List;
+
+/**
+ * User: christoph
+ * Date: 06.11.12
+ * Time: 14:09
+ */
+public interface SearchableList<E> extends List<E>, Searchable<E> {
+}
