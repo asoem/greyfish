385562d8d5331545f3d70b43f5573fc2962c9447
Christoph Hösler <christoph.hoesler@gmail.com>
2013-04-05 09:47:49 +0200

    *** HAS ERRORS ***
    Initial Idea to implement a generic property to replace simulation step / lifetime property

--- src/main/java/org/asoem/greyfish/utils/base/UpdateRequests.java
+++ src/main/java/org/asoem/greyfish/utils/base/UpdateRequests.java
@@ -33,4 +33,48 @@ public final class UpdateRequests {
             }
         };
     }
+
+    public static <T> UpdateRequest<T> once() {
+        return new UpdateOnce<T>();
+    }
+
+    private static <T> UpdateRequest<T> synchronizedRequest(UpdateRequest<T> updateRequest) {
+        return new SynchronizedRequest<T>(updateRequest);
+    }
+
+    private static class UpdateOnce<T> implements UpdateRequest<T> {
+        private boolean outdated = true;
+
+        @Override
+        public void done() {
+            outdated = false;
+        }
+
+        @Override
+        public boolean apply(@Nullable T t) {
+            return outdated;
+        }
+    }
+
+    private static class SynchronizedRequest<T> implements UpdateRequest<T> {
+        private final UpdateRequest<T> updateRequest;
+
+        public SynchronizedRequest(UpdateRequest<T> updateRequest) {
+            this.updateRequest = updateRequest;
+        }
+
+        @Override
+        public void done() {
+            synchronized (this) {
+                updateRequest.done();
+            }
+        }
+
+        @Override
+        public boolean apply(@Nullable T t) {
+            synchronized (this) {
+                return updateRequest.apply(t);
+            }
+        }
+    }
 }
