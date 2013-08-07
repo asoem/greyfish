9bd25e7b9f5074b372448f5bfc92b3e7fc85eab5
Christoph Hösler <christoph.hoesler@gmail.com>
2013-08-07 13:38:50 +0200

    Added type safe accessors

--- greyfish-core/src/main/java/org/asoem/greyfish/core/agent/Agents.java
+++ greyfish-core/src/main/java/org/asoem/greyfish/core/agent/Agents.java
@@ -1,23 +1,35 @@
 package org.asoem.greyfish.core.agent;
 
 import com.google.common.base.Predicate;
+import com.google.common.reflect.TypeToken;
 import org.asoem.greyfish.core.properties.AgentProperty;
 import org.asoem.greyfish.core.traits.AgentTrait;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
- * User: christoph
- * Date: 26.09.12
- * Time: 18:31
+ * A collection of helper functions dealing with agents.
  */
+@SuppressWarnings("UnusedDeclaration")
 public final class Agents {
 
-    private Agents() {}
+    private Agents() {
+        throw new AssertionError();
+    }
 
-    public static <A extends Agent<A, ?>, T extends AgentProperty<A, ?>> ComponentAccessor<A, T> propertyAccessor(final String propertyName) {
+    /**
+     * Create a fast accessor for properties named {@code propertyName} of agents.
+     * (Fast means faster than {@link Agent#getProperty(String)})
+     * @param propertyName the name of the property to find
+     * @param <A> the {@link Agent}'s type
+     * @param <T> the type of the property
+     * @return a new accessor instance
+     */
+    public static <A extends Agent<A, ?>, T extends AgentProperty<A, ?>>
+    ComponentAccessor<A, T> propertyAccessor(final String propertyName) {
         return new ComponentAccessor<A, T>() {
-            private final Predicate<? super AgentProperty<A, ?>> componentNamePredicate = new ComponentNamePredicate<A>(propertyName);
+            private final Predicate<? super AgentProperty<A, ?>> componentNamePredicate =
+                    new ComponentNamePredicate<A>(propertyName);
 
             @Override
             public T apply(final A input) {
@@ -26,13 +38,79 @@ public final class Agents {
         };
     }
 
-    public static <A extends Agent<A, ?>, T extends AgentTrait<A, ?>> ComponentAccessor<A, T> traitAccessor(final String traitName) {
+    /**
+     * Create a fast accessor for traits named {@code traitName} of agents.
+     * (Fast means faster than {@link Agent#getTrait(String)}
+     * @param traitName the name of the trait to find
+     * @param <A> the {@link Agent}'s type
+     * @param <T> the type of the trait
+     * @return a new accessor instance
+     * @deprecated Use {@link #traitAccessor(String, com.google.common.reflect.TypeToken)}
+     * or {@link #traitAccessor(String, Class)} instead
+     */
+    @Deprecated
+    public static <A extends Agent<A, ?>, T extends AgentTrait<A, ?>>
+    ComponentAccessor<A, T> traitAccessor(final String traitName) {
         return new ComponentAccessor<A, T>() {
-            private final Predicate<AgentComponent<A>> componentNamePredicate = new ComponentNamePredicate<A>(traitName);
+            private final Predicate<AgentComponent<A>> componentNamePredicate =
+                    new ComponentNamePredicate<A>(traitName);
 
             @Override
             public T apply(final A input) {
                 return (T) checkNotNull(input).findTrait(componentNamePredicate);
+            }
+        };
+    }
+
+    /**
+     * Create a fast accessor for traits named {@code traitName} of agents.
+     * (Fast means faster than {@link Agent#getTrait(String)}
+     * @param traitName the name of the trait to find
+     * @param tClass the expected class of the trait
+     * @param <A> the {@link Agent}'s type
+     * @param <T> the type of the trait
+     * @return a new accessor instance
+     */
+    public static <A extends Agent<A, ?>, T extends AgentTrait<A, ?>>
+    ComponentAccessor<A, T> traitAccessor(final String traitName, final Class<T> tClass) {
+        return new ComponentAccessor<A, T>() {
+            private final Predicate<AgentComponent<A>> componentNamePredicate =
+                    new ComponentNamePredicate<A>(traitName);
+
+            @Override
+            public T apply(final A input) {
+                return tClass.cast(checkNotNull(input).findTrait(componentNamePredicate));
+            }
+        };
+    }
+
+    /**
+     * Create a fast accessor for traits named {@code traitName} of agents.
+     * (Fast means faster than {@link Agent#getTrait(String)}
+     * @param traitName the name of the trait to find
+     * @param typeToken the expected type of the trait value
+     *                  {@link org.asoem.greyfish.core.traits.AgentTrait#getValueType()}
+     * @param <A> the {@link Agent}'s type
+     * @param <V> the value type of the trait
+     * @return a new accessor instance
+     */
+    public static <A extends Agent<A, ?>, V>
+    ComponentAccessor<A, AgentTrait<A, V>> traitAccessor(final String traitName, final TypeToken<V> typeToken) {
+        return new ComponentAccessor<A, AgentTrait<A, V>>() {
+            private final Predicate<AgentComponent<A>> componentNamePredicate
+                    = new ComponentNamePredicate<A>(traitName);
+
+            // Safe cast if the value type of the trait is assignable from given typeToken
+            @SuppressWarnings("unchecked")
+            @Override
+            public AgentTrait<A, V> apply(final A input) {
+                final AgentTrait<A, ?> trait = checkNotNull(input).findTrait(componentNamePredicate);
+                if (!typeToken.isAssignableFrom(trait.getValueType())) {
+                    final String message = String.format("Value type %s is not assignable from type %s",
+                            input, typeToken);
+                    throw new IllegalArgumentException(message);
+                }
+                return (AgentTrait<A, V>) trait;
             }
         };
     }
---
+++ greyfish-core/src/test/java/org/asoem/greyfish/core/agent/AgentsTest.java
@@ -0,0 +1,76 @@
+package org.asoem.greyfish.core.agent;
+
+import com.google.common.base.Predicate;
+import com.google.common.reflect.TypeToken;
+import org.asoem.greyfish.core.simulation.Simulation;
+import org.asoem.greyfish.core.traits.AgentTrait;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+
+/**
+ * User: christoph
+ * Date: 07.08.13
+ * Time: 12:12
+ */
+public class AgentsTest {
+    @Test
+    public void testTraitAccessor() throws Exception {
+        // given
+        final TestAgent agentMock = mock(TestAgent.class);
+        final AgentTrait traitMock = mock(AgentTrait.class);
+        final TypeToken<Double> valueType = new TypeToken<Double>() {};
+        given(traitMock.getValueType()).willReturn(valueType);
+        given(agentMock.findTrait(any(Predicate.class))).willReturn(traitMock);
+        final ComponentAccessor<TestAgent,AgentTrait<TestAgent,Double>> accessor
+                = Agents.traitAccessor("testTrait", valueType);
+
+        // when
+        final AgentTrait<TestAgent, Double> trait = accessor.apply(agentMock);
+
+        // then
+        assertThat(trait, is(traitMock));
+    }
+
+    @Test
+    public void testTraitAccessorDownCast() throws Exception {
+        // given
+        final TestAgent agentMock = mock(TestAgent.class);
+        final AgentTrait traitMock = mock(AgentTrait.class);
+        given(traitMock.getValueType()).willReturn(new TypeToken<Double>() {});
+        given(agentMock.findTrait(any(Predicate.class))).willReturn(traitMock);
+        final ComponentAccessor<TestAgent, AgentTrait<TestAgent, Number>> accessor
+                = Agents.traitAccessor("testTrait", new TypeToken<Number>() {});
+
+        // when
+        final AgentTrait<TestAgent, Number> trait = accessor.apply(agentMock);
+
+        // then
+        assertThat(trait, is(traitMock));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testTraitAccessorUpCastException() throws Exception {
+        // given
+        final TestAgent agentMock = mock(TestAgent.class);
+        final AgentTrait traitMock = mock(AgentTrait.class);
+        given(traitMock.getValueType()).willReturn(new TypeToken<Object>() {});
+        given(agentMock.findTrait(any(Predicate.class))).willReturn(traitMock);
+        final ComponentAccessor<TestAgent, AgentTrait<TestAgent, Number>> accessor
+                = Agents.traitAccessor("testTrait", new TypeToken<Number>() {});
+
+        // when
+        accessor.apply(agentMock);
+
+        // then
+        fail();
+    }
+
+    private static interface TestAgent extends Agent<TestAgent, TestSimulation> {}
+    private static interface TestSimulation extends Simulation<TestAgent> {}
+}
