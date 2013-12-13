5b93b2d8fd1e0f596843581e57d19880cf4d7abb
Christoph Hösler <christoph.hoesler@gmail.com>
2013-12-13 11:43:47 +0100

    Javadoc

--- greyfish-utils/src/main/java/org/asoem/greyfish/utils/math/MarkovChain.java
+++ greyfish-utils/src/main/java/org/asoem/greyfish/utils/math/MarkovChain.java
@@ -8,13 +8,12 @@ import java.util.Set;
 public interface MarkovChain<S> {
 
     /**
-     *
      * @return all registered states
      */
     Set<S> getStates();
 
     /**
-     * Make a transition to the next state as defined by the markovMatrix
+     * Calculate the transition to the next state given the current {@code state}.
      *
      * @param state the current state
      * @return the next state
