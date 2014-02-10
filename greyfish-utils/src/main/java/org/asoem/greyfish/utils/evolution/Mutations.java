d9e267f23d8ebc33098004c620f2e00f97671cb8
Christoph Hösler <christoph.hoesler@gmail.com>
2014-02-10 11:49:25 +0100

    Javadoc

--- greyfish-utils/src/main/java/org/asoem/greyfish/utils/evolution/Mutations.java
+++ greyfish-utils/src/main/java/org/asoem/greyfish/utils/evolution/Mutations.java
@@ -7,9 +7,20 @@ import org.asoem.greyfish.utils.collect.BitString;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
+/**
+ * A utility class for accessing different implementations of {@link org.asoem.greyfish.utils.evolution.Mutation}.
+ */
 public final class Mutations {
     private Mutations() {}
 
+    /**
+     * Create a new bit-flip mutation function. This function flips every bit in a given bit string with probability
+     * {@code p}.
+     *
+     * @param rng the random generator to use.
+     * @param p   the probability to flip a bit
+     * @return a new flip-bit mutation function
+     */
     public static Mutation<BitString> bitFlipMutation(final RandomGenerator rng, final double p) {
         checkNotNull(rng);
         checkArgument(p >= 0 && p <= 1);
